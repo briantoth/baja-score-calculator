ced4d1ea5862b96ff9795b0a74866fa04dd2a3ee
Brian Toth <bri.toth@gmail.com>
2014-04-27 13:23:20 -0700

    Some stuff on sales.

--- sales.py
+++ sales.py
@@ -1,5 +1,10 @@
 from column_parser import parse_columns
 
+def get_sales_score():
+    results = parse_columns("./el_paso_4-27-14/sales.tsv")
+    for result in results.values():
+        print result
+
 if __name__ == "__main__":
-    print(parse_columns("./el_paso_4-27-14/sales.tsv"))
+    get_sales_score()
 
